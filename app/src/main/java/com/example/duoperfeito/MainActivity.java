fb8a9410b9534f87e0d6e134b0e67ae0288dfb2d
rafael-junio <rafael-954@hotmail.com>
2021-11-12 16:18:50 -0400

    Interação básica da tela inicial implementada

--- app/src/main/java/com/example/duoperfeito/MainActivity.java
+++ app/src/main/java/com/example/duoperfeito/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.duoperfeito;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.duoperfeito.empresario.EmpLoginActivity;
+import com.example.duoperfeito.profissional.ProfLoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButtons();
+    }
+
+//    TODO: Da para fazer usando uma única tela de login, mas para facilitar fiz duas mesmo
+    private void startButtons() {
+        Button btnEmp = findViewById(R.id.btnEmp);
+        Button btnProf = findViewById(R.id.btnProf);
+
+        btnEmp.setOnClickListener(view -> {
+            Intent it = new Intent(MainActivity.this, EmpLoginActivity.class);
+            startActivity(it);
+        });
+        btnProf.setOnClickListener(view -> {
+            Intent it = new Intent(MainActivity.this, ProfLoginActivity.class);
+            startActivity(it);
+        });
     }
 }
