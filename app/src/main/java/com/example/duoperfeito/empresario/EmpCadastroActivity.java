af3a766c43cdcb8dae232240813f06b78af27bbb
rafael-junio <rafael-954@hotmail.com>
2021-11-17 13:42:41 -0400

    Cadastro do empresário adicionado

--- app/src/main/java/com/example/duoperfeito/empresario/EmpCadastroActivity.java
+++ app/src/main/java/com/example/duoperfeito/empresario/EmpCadastroActivity.java
@@ -3,14 +3,55 @@ package com.example.duoperfeito.empresario;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.duoperfeito.R;
+import com.example.duoperfeito.database.DuoPerfeitoDatabase;
+import com.example.duoperfeito.database.dao.EmpresarioDAO;
+import com.example.duoperfeito.model.Empresario;
+import com.example.duoperfeito.repository.EmpresarioRepository;
 
 public class EmpCadastroActivity extends AppCompatActivity {
+
+    EditText nome, telefone, email, cpf, senha;
+    Button cadastrar;
+    private EmpresarioRepository empresarioRepository;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emp_cadastro);
+
+
+        nome = findViewById(R.id.edtTxtNome);
+        telefone = findViewById(R.id.edtTxtTelefone);
+        email = findViewById(R.id.edtTxtEmail);
+        senha = findViewById(R.id.edtTxtSenha);
+        cpf = findViewById(R.id.edtTxtCpf);
+        cadastrar = findViewById(R.id.btnCadastro);
+
+        empresarioRepository = new EmpresarioRepository(this);
+        cadastrar.setOnClickListener(view -> {
+            Empresario empresario = new Empresario();
+            empresario.setNome(nome.getText().toString());
+            empresario.setSobrenome(nome.getText().toString());
+            empresario.setEmail(email.getText().toString());
+            empresario.setTelefone(telefone.getText().toString());
+            empresario.setSenha(senha.getText().toString());
+            empresario.setCpf(cpf.getText().toString());
+            empresarioRepository.salva(empresario, new EmpresarioRepository.DadosCarregadosCallback<Empresario>() {
+                @Override
+                public void quandoSucesso(Empresario resultado) {
+                    Toast.makeText(getApplicationContext(), "Empresário cadastrado!", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void quandoFalha(String erro) {
+                    Toast.makeText(getApplicationContext(), "Preencha corretamente os campos!", Toast.LENGTH_SHORT).show();
+                }
+            });
+        });
     }
 }
--- app/src/main/java/com/example/duoperfeito/empresario/EmpLoginActivity.java
+++ app/src/main/java/com/example/duoperfeito/empresario/EmpLoginActivity.java
@@ -2,9 +2,13 @@ package com.example.duoperfeito.empresario;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
+import com.example.duoperfeito.MainActivity;
 import com.example.duoperfeito.R;
+import com.example.duoperfeito.profissional.ProfLoginActivity;
 
 public class EmpLoginActivity extends AppCompatActivity {
 
@@ -12,5 +16,21 @@ public class EmpLoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emp_login);
+
+        startButtons();
+    }
+
+    private void startButtons() {
+        Button btnLogin = findViewById(R.id.btnLogin);
+        Button btnCadastro = findViewById(R.id.btnCadastro);
+
+        btnLogin.setOnClickListener(view -> {
+            Intent it = new Intent(EmpLoginActivity.this, EmpMainActivity.class);
+            startActivity(it);
+        });
+        btnCadastro.setOnClickListener(view -> {
+            Intent it = new Intent(EmpLoginActivity.this, EmpCadastroActivity.class);
+            startActivity(it);
+        });
     }
 }
